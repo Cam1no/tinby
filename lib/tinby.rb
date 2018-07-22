e4678b021574f3c34cc72c48e9bbff45f9d7d8f5
joe <kinjoyuji@gmail.com>
2018-07-22 20:37:33 +0900

    fix facebook

--- lib/tinby.rb
+++ lib/tinby.rb
@@ -1,10 +1,1 @@
-require 'tinby/version'
-
-module Tinby
-  def self.greet
-    'Hello World!'
-  end
-end
-
-Gem.find_files('tinderbot/*.rb').each { |file| require file }
-Gem.find_files('tinderbot/**/*.rb').each { |file| require file }
+Gem.find_files('tinby/**/*.rb').each { |file| require file }
--- lib/tinby/facebook.rb
+++ lib/tinby/facebook.rb
@@ -1,11 +1,13 @@
+require 'tinder_auth_fetcher'
+
 module Tinby
-  class Facebook
+  module Facebook
     FIND_FACEBOOK_ID_URL = 'https://findmyfbid.com'.freeze
 
-    def get_facebook_token(email, password)
+    def self.get_facebook_token(email, password)
       TinderAuthFetcher.fetch_token(email, password)
     end
 
-    def get_facebook_id(profile_url); end
+    def self.get_facebook_id(profile_url); end
   end
 end
