dbbe564125140c847047e25b105a772b86ba8a9e
joe <kinjoyuji@gmail.com>
2018-07-22 20:25:29 +0900

    Add Facebook module

---
+++ lib/tinby/facebook.rb
@@ -0,0 +1,11 @@
+module Tinby
+  module Facebook
+    FIND_FACEBOOK_ID_URL = 'https://findmyfbid.com'.freeze
+
+    def get_facebook_token(email, password)
+      TinderAuthFetcher.fetch_token(email, password)
+    end
+
+    def get_facebook_id(profile_url); end
+  end
+end
