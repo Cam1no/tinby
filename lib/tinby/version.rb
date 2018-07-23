d05c030ce0a3151fad0bae5fb73f10621c37080c
joe <kinjoyuji@gmail.com>
2018-07-24 01:15:23 +0900

    versions 0.1.2

--- lib/tinby/version.rb
+++ lib/tinby/version.rb
@@ -1,3 +1,3 @@
 module Tinby
-  VERSION = '0.1.1'.freeze
+  VERSION = '0.1.2'.freeze
 end
--- spec/tinby_spec.rb
+++ spec/tinby_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Tinby do
   it 'has a version number' do
     expect(Tinby::VERSION).not_to be nil
   end
-
-  it '.greet' do
-    expect(Tinby.greet).to eq 'Hello World!'
-  end
 end
